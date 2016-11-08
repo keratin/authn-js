70e6e4ef7239df7623ce41d52352e315bbf23277
Lance Ivy <lance@cainlevy.net>
2016-11-08 14:49:49 -0800

    convert to typescript

---
+++ index.ts
@@ -0,0 +1,57 @@
+'use strict';
+
+import Promise = require('promise');
+
+interface Credentials {
+  username: string,
+  password: string
+}
+
+interface AuthAPI {
+  ISSUER: string,
+  inflight: boolean,
+  accounts_url(this: AuthAPI): string,
+  signup(this: AuthAPI, credentials: Credentials): Promise.IThenable<string>
+}
+
+export const AuthAPI = {
+  ISSUER: '',
+  inflight: false,
+
+  accounts_url: function accounts_url(this: AuthAPI): string {
+    if (!this.ISSUER.length) {
+      throw "AuthAPI.ISSUER not set";
+    }
+    return `${this.ISSUER}/accounts`;
+  },
+
+  signup: function signup(this: AuthAPI, credentials: Credentials): Promise.IThenable<string> {
+    const formData: string = `username=${encodeURIComponent(credentials.username)}&password=${encodeURIComponent(credentials.password)}`;
+
+    return new Promise(function (fulfill, reject) {
+      if (AuthAPI.inflight) {
+        reject("duplicate");
+        return;
+      } else {
+        AuthAPI.inflight = true;
+      }
+
+      const xhr = new XMLHttpRequest();
+      xhr.onreadystatechange = function() {
+        if (xhr.readyState == XMLHttpRequest.DONE) {
+          const data = JSON.parse(xhr.responseText);
+          if (data.result) {
+            fulfill(data.result.id_token);
+          } else if (data.errors) {
+            reject(data.errors);
+          } else {
+            reject('unknown response from server');
+          }
+          AuthAPI.inflight = false;
+        }
+      };
+      xhr.open("POST", AuthAPI.accounts_url());
+      xhr.send(formData);
+    });
+  }
+};
