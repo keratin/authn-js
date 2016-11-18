11b5eca4d877d4fefa68f84b411d08d8b35eea95
Lance Ivy <lance@cainlevy.net>
2016-11-18 13:53:35 -0800

    add uglified .min output
    node_modules is up to 34mb

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,14 @@
 var gulp = require('gulp');
 var ts = require('gulp-typescript');
 var tsProject = ts.createProject('tsconfig.json');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
 
 gulp.task('default', function() {
   return tsProject.src()
     .pipe(tsProject())
-    .js.pipe(gulp.dest('dist'));
+    .js.pipe(gulp.dest('dist'))
+    .pipe(uglify({ mangle: true }))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('dist'));
 });
