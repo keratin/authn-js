15710c5a4d73d698f8bbd2cdbafdc33a2c98ad08
Lance Ivy <lance@cainlevy.net>
2016-11-21 20:32:02 -0800

    add browserify to gulp build

--- dist/keratin-authn.js
+++ dist/keratin-authn.js
@@ -1,3 +1,4 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
 var verbs_1 = require("./verbs");
 var KeratinAuthN = {
@@ -42,3 +43,44 @@ function formDataItem(k, v) {
     return k + "=" + encodeURIComponent(v);
 }
 var inflight = false;
+
+},{"./verbs":2}],2:[function(require,module,exports){
+"use strict";
+function get(url, queryString) {
+    return jhr(function (xhr) {
+        xhr.open("GET", url + "?" + queryString);
+        xhr.send();
+    });
+}
+exports.get = get;
+function post(url, formData) {
+    return jhr(function (xhr) {
+        xhr.open("POST", url);
+        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        xhr.send(formData);
+    });
+}
+exports.post = post;
+function jhr(sender) {
+    return new Promise(function (fulfill, reject) {
+        var xhr = new XMLHttpRequest();
+        xhr.withCredentials = true; // enable authentication server cookies
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState == XMLHttpRequest.DONE) {
+                var data = (xhr.responseText.length > 1) ? JSON.parse(xhr.responseText) : {};
+                if (data.result) {
+                    fulfill(data.result);
+                }
+                else if (data.errors) {
+                    reject(data.errors);
+                }
+                else {
+                    reject(xhr.statusText);
+                }
+            }
+        };
+        sender(xhr);
+    });
+}
+
+},{}]},{},[1]);
--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,21 @@
+var browserify = require("browserify");
+var buffer = require('vinyl-buffer');
 var gulp = require('gulp');
-var ts = require('gulp-typescript');
-var tsProject = ts.createProject('tsconfig.json');
-var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var source = require('vinyl-source-stream');
+var tsify = require("tsify");
+var uglify = require('gulp-uglify');
 
 gulp.task('default', function() {
-  return tsProject.src()
-    .pipe(tsProject())
-    .js.pipe(gulp.dest('dist'))
+  return browserify({
+      basedir: '.',
+      entries: ['src/keratin-authn.ts']
+    })
+    .plugin(tsify)
+    .bundle()
+    .pipe(source('keratin-authn.js'))
+    .pipe(gulp.dest("dist"))
+    .pipe(buffer())
     .pipe(uglify({ mangle: true }))
     .pipe(rename({ suffix: '.min' }))
     .pipe(gulp.dest('dist'));
