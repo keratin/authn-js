c63f07d2e6f45473160ea02cf6c6250514acbdd7
Lance Ivy <lance@cainlevy.net>
2017-01-20 12:23:57 -0800

    compile before browserify
    i want my distribution to contain library files, too

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,14 @@
 var browserify = require("browserify");
 var buffer = require('vinyl-buffer');
+var exec = require('child_process').exec;
 var gulp = require('gulp');
 var rename = require('gulp-rename');
 var source = require('vinyl-source-stream');
-var tsify = require("tsify");
 var uglify = require('gulp-uglify');
 var qunit = require('gulp-qunit');
 
 function build(extension) {
-  let src = extension ? `src/main.${extension}.ts` : `src/main.ts`;
+  let src = extension ? `lib/main.${extension}.js` : `lib/main.js`;
   let dest = extension ? `keratin-authn.${extension}.js` : `keratin-authn.js`;
 
   return browserify({
@@ -16,7 +16,6 @@ function build(extension) {
     entries: [src],
     standalone: 'KeratinAuthN'
   })
-  .plugin(tsify)
   .bundle()
   .pipe(source(dest))
   .pipe(gulp.dest("dist"))
@@ -26,11 +25,11 @@ function build(extension) {
   .pipe(gulp.dest('dist'));
 }
 
-gulp.task('build-core', function() {
+gulp.task('build-core', ['compile'], function() {
   return build();
 });
 
-gulp.task('build-cookie', function() {
+gulp.task('build-cookie', ['compile'], function() {
   return build('cookie');
 });
 
@@ -39,4 +38,12 @@ gulp.task('default', ['build-core', 'build-cookie']);
 gulp.task('test', ['build-core', 'build-cookie'], function () {
   return gulp.src('./test/runner.html')
     .pipe(qunit());
+});
+
+gulp.task('compile', function (cb) {
+  exec('tsc', function (err, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
+    cb(err);
+  });
 });
